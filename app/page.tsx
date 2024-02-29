76236bc46f1d2189f805d8b37bf5773c58979018
Rijosh <rijosh.ravi@phases.io>
2024-02-29 09:41:16 +0530

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
-
-export default function Home() {
+function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Hello there</h1>
+    </main>
   );
 }
+
+export default Home;
